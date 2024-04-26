c676c6f892a51e73fcfdd0c2188a3856e6c788b7
implObserver <prognozman@mail.ru>
2024-04-26 13:50:35 +0500

    feat:add feature slice design slices

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './state/store'
-console.log(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
